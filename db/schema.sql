1673436198bfdbfcf895ad3629507d77e49be027
Haydee Shane Saguid <137850019+Yahds@users.noreply.github.com>
2026-09-15 21:16:21 +0800

    combine db dumps into separate schema and seed

---
+++ db/schema.sql
@@ -0,0 +1,209 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: ojt
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `advisers`
+--
+
+DROP TABLE IF EXISTS `advisers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `advisers` (
+  `adviserID` int NOT NULL,
+  `adviserName` varchar(45) NOT NULL,
+  `adviserEmail` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `image` blob,
+  PRIMARY KEY (`adviserID`),
+  UNIQUE KEY `adviserID_UNIQUE` (`adviserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `announcements`
+--
+
+DROP TABLE IF EXISTS `announcements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `announcements` (
+  `announcementid` int NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `senderid` varchar(45) NOT NULL,
+  `recipientid` int NOT NULL,
+  `subject` varchar(45) NOT NULL,
+  `message` varchar(45) NOT NULL,
+  PRIMARY KEY (`announcementid`),
+  UNIQUE KEY `announcementid_UNIQUE` (`announcementid`)
+) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `company` (
+  `companyid` int NOT NULL,
+  `companyname` varchar(45) DEFAULT NULL,
+  `companyaddress` varchar(45) DEFAULT NULL,
+  `companytype` varchar(45) DEFAULT 'P, G',
+  PRIMARY KEY (`companyid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dailyreports`
+--
+
+DROP TABLE IF EXISTS `dailyreports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dailyreports` (
+  `reportid` int NOT NULL,
+  `internid` int NOT NULL,
+  `companyid` int NOT NULL,
+  `supervisorid` int NOT NULL,
+  `date` date NOT NULL,
+  `timeIn` time DEFAULT NULL,
+  `timeOut` time DEFAULT NULL,
+  `hours` int DEFAULT NULL,
+  `workdescription` varchar(45) DEFAULT NULL,
+  `verificationstatus` varchar(45) DEFAULT 'PENDING',
+  `remark` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`reportid`,`internid`),
+  UNIQUE KEY `reportid_UNIQUE` (`reportid`),
+  KEY `companyid_idx` (`companyid`),
+  KEY `internid_idx` (`internid`),
+  KEY `supervisor_id_idx` (`supervisorid`),
+  CONSTRAINT `company_id` FOREIGN KEY (`companyid`) REFERENCES `company` (`companyid`),
+  CONSTRAINT `super_id` FOREIGN KEY (`supervisorid`) REFERENCES `supervisors` (`supervisorid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `internrequirements`
+--
+
+DROP TABLE IF EXISTS `internrequirements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `internrequirements` (
+  `internid` int NOT NULL,
+  `reqid` int NOT NULL,
+  `datesubmitted` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `remarks` varchar(200) DEFAULT NULL,
+  KEY `internid_fk_idx` (`internid`),
+  KEY `reqid_fk_idx` (`reqid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `interns`
+--
+
+DROP TABLE IF EXISTS `interns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `interns` (
+  `internid` int NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `adviserid` int NOT NULL,
+  `studentid` int NOT NULL,
+  `companyid` int DEFAULT NULL,
+  `supervisorid` int DEFAULT NULL,
+  `totalhours` int DEFAULT NULL,
+  `worktype` varchar(45) DEFAULT NULL,
+  `image` blob,
+  `status` varchar(45) DEFAULT 'PENDING',
+  PRIMARY KEY (`internid`),
+  UNIQUE KEY `internid_UNIQUE` (`internid`),
+  KEY `adviserid_idx` (`adviserid`),
+  KEY `studentid_idx` (`studentid`),
+  KEY `companyid_idx` (`companyid`),
+  KEY `supervisorid_idx` (`supervisorid`),
+  CONSTRAINT `adviserid` FOREIGN KEY (`adviserid`) REFERENCES `advisers` (`adviserID`),
+  CONSTRAINT `companyid` FOREIGN KEY (`companyid`) REFERENCES `company` (`companyid`),
+  CONSTRAINT `studentid` FOREIGN KEY (`studentid`) REFERENCES `students` (`studentID`),
+  CONSTRAINT `supervisorid` FOREIGN KEY (`supervisorid`) REFERENCES `supervisors` (`supervisorid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `requirements`
+--
+
+DROP TABLE IF EXISTS `requirements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `requirements` (
+  `reqid` int NOT NULL,
+  `requirementname` varchar(45) NOT NULL,
+  PRIMARY KEY (`reqid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `studentID` int NOT NULL,
+  `studentName` varchar(45) NOT NULL,
+  `course` varchar(45) NOT NULL,
+  `year` varchar(45) NOT NULL,
+  `classcode` varchar(45) NOT NULL,
+  PRIMARY KEY (`studentID`),
+  UNIQUE KEY `studentID_UNIQUE` (`studentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `supervisors`
+--
+
+DROP TABLE IF EXISTS `supervisors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supervisors` (
+  `supervisorid` int NOT NULL,
+  `supervisorname` varchar(45) DEFAULT NULL,
+  `supervisoremail` varchar(45) DEFAULT NULL,
+  `position` varchar(45) DEFAULT NULL,
+  `companyid` int DEFAULT NULL,
+  PRIMARY KEY (`supervisorid`),
+  KEY `companyid_idx` (`companyid`),
+  CONSTRAINT `companid` FOREIGN KEY (`companyid`) REFERENCES `company` (`companyid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-09-15 12:55:59
